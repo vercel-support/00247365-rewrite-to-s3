dd880e4f51d7fab1252902711184410962a72da2
aldosch <git@aldo.io>
2024-06-21 18:22:07 +1000

    refactor file extension handling into one rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -5,34 +5,9 @@ const nextConfig = {
     return {
       beforeFiles: [
         {
-          source: "/audits/:path*.svg",
+          source: "/audits/:path*.:ext(svg|png|jpeg|jpg|css|js)",
           destination:
-            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.svg",
-        },
-        {
-          source: "/audits/:path*.png",
-          destination:
-            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.png",
-        },
-        {
-          source: "/audits/:path*.jpeg",
-          destination:
-            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.jpeg",
-        },
-        {
-          source: "/audits/:path*.jpg",
-          destination:
-            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.jpg",
-        },
-        {
-          source: "/audits/:path*.css",
-          destination:
-            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.css",
-        },
-        {
-          source: "/audits/:path*.js",
-          destination:
-            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.js",
+            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.:ext",
         },
         {
           source: "/audits",
