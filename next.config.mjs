e8233c42298a664c1ccc24aec2b1d3b120cc4eb5
aldosch <git@aldo.io>
2024-06-21 14:25:55 +1000

    revise rewrites to handle files and append /index.html to child routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,51 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   async rewrites() {
-    return [
-      {
-        source: "/audits/:path*",
-        destination:
-          "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*",
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: "/audits/:path*.svg",
+          destination:
+            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.svg",
+        },
+        {
+          source: "/audits/:path*.png",
+          destination:
+            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.png",
+        },
+        {
+          source: "/audits/:path*.jpeg",
+          destination:
+            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.jpeg",
+        },
+        {
+          source: "/audits/:path*.jpg",
+          destination:
+            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.jpg",
+        },
+        {
+          source: "/audits/:path*.css",
+          destination:
+            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.css",
+        },
+        {
+          source: "/audits/:path*.js",
+          destination:
+            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*.js",
+        },
+        {
+          source: "/audits",
+          destination:
+            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com",
+        },
+        {
+          source: "/audits/:path*",
+          destination:
+            "http://dedaub-audit-reports.s3-website.eu-central-1.amazonaws.com/:path*/index.html",
+        },
+      ],
+    };
   },
 };
 
